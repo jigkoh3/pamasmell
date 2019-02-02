5fc04e285641009c9a908600c01d5e406a752883
Theerasak Tubrit <jigkoh3@Theerasaks-MacBook-Pro.local>
2019-02-02 08:53:27 +0700

    sort by date

--- src/modules/pmreport/controllers/controller.js
+++ src/modules/pmreport/controllers/controller.js
@@ -245,7 +245,7 @@ exports.cookTemplateData = (req, res, next) => {
                 max = element.aqi;
                 req.columns.push({
                     title: element.aqi,
-                    text: `${element.name} \n min:${min} | max:${max} Last Update: `,
+                    text: `${element.name} \n min:${min} | max:${max}`,
                     min: element.aqi,
                     max: element.aqi,
                     sum: element.aqi,
